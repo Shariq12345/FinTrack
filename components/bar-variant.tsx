49f7b17db5ec89caf1fa95b986eb6880303b06d1
shariq <shariqshaikh11042004@gmail.com>
2024-06-16 00:56:11 +0530

    Implmented Charts

---
+++ components/bar-variant.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { format } from "date-fns";
+import {
+  Tooltip,
+  XAxis,
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  CartesianGrid,
+} from "recharts";
+import { CustomTooltip } from "./custom-tooltip";
+
+type Props = {
+  data: {
+    date: string;
+    income: number;
+    expenses: number;
+  }[];
+};
+
+export const BarVariant = ({ data }: Props) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          axisLine={false}
+          dataKey="date"
+          tickLine={false}
+          tickFormatter={(value) => format(new Date(value), "dd MMM")}
+          style={{ fontSize: "12px" }}
+          tickMargin={10}
+        />
+        <Tooltip content={<CustomTooltip />} />
+        <Bar
+          dataKey="income"
+          stackId="income"
+          fill="#10B981"
+          className="drop-shadow-sm"
+        />
+        <Bar
+          dataKey="expenses"
+          stackId="expenses"
+          fill="#EF4444"
+          className="drop-shadow-sm"
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
